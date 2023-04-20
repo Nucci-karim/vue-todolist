446172c0afb3a0c661f6a893afd40c5b17ea5906
Nucci-karim <nuccikarim@gmail.com>
2023-04-20 15:53:06 +0200

    lista funzionale

--- assets/js/main.js
+++ assets/js/main.js
@@ -16,10 +16,30 @@ const { createApp } = Vue
                 text: 'Fare il bucato',
                 done: false
             }
+        
         ],
+        addText: '',
+        error: false,
       }
     },
     methods: {
-        
+        // creazione riga della lista 
+        creaTodo(){
+            if(this.addText != ''){
+                // metto l'oggetto in una variabile per pusharlo 
+                let task = {
+                    text: this.addText,
+                    done: false
+                }
+                this.todos.push(task);
+                this.error = false
+            }else {
+                this.error = true
+            }
+        },
+        // funzione per cancelare la riga con la x
+        cancella(i){
+            this.todos.splice(i, 1)
+        },
     },
   }).mount('#app')
